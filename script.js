dafc3788a6fd98b6b121de53400412a88b29772f
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-06-08 22:33:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function rot13(encodedStr) {
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
- console.log(rot13());
+ console.log(rot13("EBG13 rknzcyr.");
 
 // Do not change this line
 window.rot13 = rot13;
